d3f68f51ca9f945cc82d12e0345430908c328e19
Akouvi Kouedjin <kouedjinl@gmail.com>
2022-05-12 19:32:54 -0400

    commited form components

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,37 @@
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
 import {
+  // auth,
   signInWithGooglePopup,
+  // signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await getRedirectResult(auth);
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-
-    // console.log(response);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}> Sign In with Google</button>
+      <button onClick={logGoogleUser}> Sign In with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button> */}
+      <SignUpForm />
     </div>
   );
 };
